ace5291646265eef018889c6aa8983d141e53838
jdeepee <joshuadparkin@gmail.com>
2021-06-08 18:08:04 +0100

    integrate with new ad4m version and remove ad4m-language-context

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import type Address from "@perspect3vism/ad4m/Address";
 import type Agent from "@perspect3vism/ad4m/Agent";
 import type Language from "@perspect3vism/ad4m/Language";
-import type LanguageContext from "@perspect3vism/ad4m-language-context/LanguageContext";
+import type LanguageContext from "@perspect3vism/ad4m/LanguageContext";
+import type { HolochainLanguageDelegate } from "@perspect3vism/ad4m/LanguageContext";
 import type { Interaction } from "@perspect3vism/ad4m/Language";
 import ProfileAdapter from "./adapter";
 import ProfileAuthorAdapter from "./authorAdapter";
@@ -9,7 +10,6 @@ import Icon from "./build/Icon.js";
 import ConstructorIcon from "./build/ConstructorIcon.js";
 import { ProfileExpressionUI } from "./profileExpressionUI";
 import { DNA, DNA_NICK } from "./dna";
-import type HolochainLanguageDelegate from "@perspect3vism/ad4m-language-context/Holochain/HolochainLanguageDelegate";
 
 function iconFor(expression: Address): string {
   return Icon;
