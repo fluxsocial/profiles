4752257f57e5ae768e1c08dd08ddc7fdf8b11a3f
jdeepee <joshuadparkin@gmail.com>
2022-01-22 18:49:46 +0000

    bump hdk to 119

--- hc-dna/default.nix
+++ hc-dna/default.nix
@@ -1,5 +1,5 @@
 {
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/2f8ca2fa76165e2978112cb693c572f1086c5541.tar.gz"; }
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/2f7b8047d6314f64fca34394a52d465c18b2f4d5.tar.gz"; }
 }:
 
 let
